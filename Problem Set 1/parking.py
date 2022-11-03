5fcc8b9df6d3c87edd4cb9542691d5d9ea0f39bf
MUHAMMAD SAAD <61880555+Muhammad-saad-2000@users.noreply.github.com>
2022-11-03 16:29:54 +0000

    Q1 done

--- Problem Set 1/parking.py
+++ Problem Set 1/parking.py
@@ -3,7 +3,7 @@ from problem import Problem
 from mathutils import Direction, Point
 from helpers import utils
 
-#TODO: (Optional) Instead of Any, you can define a type for the parking state
+# COMMENT: I chose point because the car locations are Points and the state is just the location of cars:)
 ParkingState = Tuple[Point]
 # An action of the parking problem is a tuple containing an index 'i' and a direction 'd' where car 'i' should move in the direction 'd'.
 ParkingAction = Tuple[int, Direction]
@@ -19,30 +19,52 @@ class ParkingProblem(Problem[ParkingState, ParkingAction]):
 
     # This function should return the initial state
     def get_initial_state(self) -> ParkingState:
-        #TODO: ADD YOUR CODE HERE
-        utils.NotImplemented()
+        # COMMENT: The cars locations are the things the diffrentiate the states
+        return self.cars
     
     # This function should return True if the given state is a goal. Otherwise, it should return False.
     def is_goal(self, state: ParkingState) -> bool:
-        #TODO: ADD YOUR CODE HERE
-        utils.NotImplemented()
+        # COMMENT: The goal is when all the cars are in their parking slots
+        for car_index, _ in enumerate(state):
+            if not(state[car_index]in self.slots) or self.slots[state[car_index]] != car_index:
+                return False
+        return True
     
     # This function returns a list of all the possible actions that can be applied to the given state
     def get_actions(self, state: ParkingState) -> List[ParkingAction]:
-        #TODO: ADD YOUR CODE HERE
-        utils.NotImplemented()
+        #COMMENT: Loop over all the cars and test all the directions and return the valid ones (empty location that has no walls and is not another car)
+        possible_actions = []
+        for car_index, car_location in enumerate(state):
+            for direction in Direction:
+                new_car_location = car_location + direction.to_vector()
+                if new_car_location in self.passages and new_car_location not in state:
+                    possible_actions.append((car_index, direction))
+        return possible_actions
     
     # This function returns a new state which is the result of applying the given action to the given state
     def get_successor(self, state: ParkingState, action: ParkingAction) -> ParkingState:
-        #TODO: ADD YOUR CODE HERE
-        utils.NotImplemented()
+        # COMMENT: Move the car in the action in the direction of the action
+        successor = []
+        for car_index, car_location in enumerate(state):
+            if car_index == action[0]:
+                successor.append(car_location + action[1].to_vector())
+            else:
+                successor.append(car_location)
+        return tuple(successor)
     
     # This function returns the cost of applying the given action to the given state
     def get_cost(self, state: ParkingState, action: ParkingAction) -> float:
-        #TODO: ADD YOUR CODE HERE
-        utils.NotImplemented()
+        # COMMENT: The cost is 1 for every move and if the car moves to another car's parking slot it is 100
+        cost = 1
+        car_index, direction = action
+        car_location = state[car_index]
+        new_car_location = car_location + direction.to_vector()
+        if new_car_location in self.slots and self.slots[new_car_location] != car_index:
+            cost += 100
+        return cost
+
     
-     # Read a parking problem from text containing a grid of tiles
+    # Read a parking problem from text containing a grid of tiles
     @staticmethod
     def from_text(text: str) -> 'ParkingProblem':
         passages =  set()
