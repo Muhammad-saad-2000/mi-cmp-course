46856b6de3d0c271290c179ad84d08806ff55bfc
MUHAMMAD SAAD <61880555+Muhammad-saad-2000@users.noreply.github.com>
2022-11-14 19:04:53 +0000

    Finalizing req 2

--- Problem Set 1/dungeon_heuristic.py
+++ Problem Set 1/dungeon_heuristic.py
@@ -7,193 +7,37 @@ from dataclasses import dataclass
 def weak_heuristic(problem: DungeonProblem, state: DungeonState):
     return euclidean_distance(state.player, problem.layout.exit)
 
-#TODO: Import any modules and write any functions you want to use
-
 def strong_heuristic(problem: DungeonProblem, state: DungeonState) -> float:
-    #TODO: ADD YOUR CODE HERE
-    #IMPORTANT: DO NOT USE "problem.is_goal" HERE.
-    # Calling it here will mess up the tracking of the explored nodes count
-    # which is considered the number of is_goal calls during the search
-    #NOTE: you can use problem.cache() to get a dictionary in which you can store information that will persist between calls of this function
-    # This could be useful if you want to store the results heavy computations that can be cached and used across multiple calls of this function
-    #Solution1:
-    #return manhattan_distance(state.player, problem.layout.exit)
-    #Solution2:
-    # state_cost = None
-    # for coin_point in state.remaining_coins:
-    #     distance = manhattan_distance(state.player, coin_point)
-    #     if state_cost is None or distance > state_cost:
-    #         state_cost = distance
-    # distance = manhattan_distance(state.player, problem.layout.exit)
-    # if state_cost is None or distance > state_cost:
-    #     state_cost = distance
-    # return state_cost
-    #Solution3:
-    # source = state.player
-    # target = problem.layout.exit
-    # max_path_cost=None
-    # for coin in state.remaining_coins:
-    #     path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
-    #     if max_path_cost is None or path_cost > max_path_cost:
-    #         max_path_cost = path_cost
-    # if max_path_cost is None:
-    #     max_path_cost = manhattan_distance(source, target)
-    # return max_path_cost
-    #Solution4:
-    # source = state.player
-    # target = problem.layout.exit
-    # min_path_cost=None
-    # data_size = len(state.remaining_coins)
-    # if data_size > 1:
-    #     for coin_1 in state.remaining_coins:
-    #         for coin_2 in state.remaining_coins - {coin_1}:
-    #             path_cost = manhattan_distance(source, coin_1) + manhattan_distance(coin_1, coin_2) + manhattan_distance(coin_2, target)
-    #             if min_path_cost is None or path_cost < min_path_cost:
-    #                 min_path_cost = path_cost
-    # elif data_size > 0:
-    #     for coin in state.remaining_coins:
-    #         path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
-    #         if min_path_cost is None or path_cost < min_path_cost:
-    #             min_path_cost = path_cost
-    #         if min_path_cost is None:
-    #             min_path_cost = manhattan_distance(source, target)
-    # if min_path_cost is None:
-    #     min_path_cost = manhattan_distance(source, target)
-    # return min_path_cost
-    #Solution5:avg
-    # source = state.player
-    # target = problem.layout.exit
-    # avg_path_cost=0
-    # data_size = len(state.remaining_coins)
-    # for coin in state.remaining_coins:
-    #     path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
-    #     avg_path_cost += path_cost
-    # avg_path_cost += manhattan_distance(source, target)
-    # return int(avg_path_cost / (data_size + 1))
-    #Solution6:
-    # source = state.player
-    # target = problem.layout.exit
-    # max_path_cost = 0
-    # data_size = len(state.remaining_coins)
+    #COMMENT: (How I thought about the problem)
+    #First thing first, Manhattan distance is the best distace metric to be use for this problem.
+    #Getting an addmissible heuristic is super easy, the problem is for it to be consistent is a whole other story.
+    #The best intuitive realaxed version is the one that has no walls.
+    #The most important observation for consistency is that the one step cost is always 1.
+    #Thus the huristic has to change by at most 1 for every step.
+    #Thus if you are summing multiple distances the sum of the distances can only increase by 1 for every step.
+    #Thus if one increases the other has to stay the same.
+    #Given all the consistant heuristics that esstimtes the distance between the player and the exit using one coin, only one could be used at a time (otherwise the sum would increase by more than 1).
+    #The best one coin heuristic is coin that creates the maximum distance between the player and the exit.
+    #All that applies to one coin also applies to multiple coins.
+    #The coins location and the exit are fixed so the best pathes between them could be precomputed.
+    #ALL of the upove could be generalized to any number of coins.
 
-    # if data_size > 1:
-    #     for coin_1 in state.remaining_coins:
-    #         for coin_2 in state.remaining_coins - {coin_1}:
-    #             path_cost_1 = manhattan_distance(source, coin_1) + manhattan_distance(coin_1, coin_2) + manhattan_distance(coin_2, target)
-    #             path_cost_2 = manhattan_distance(source, coin_2) + manhattan_distance(coin_1, coin_2) + manhattan_distance(coin_1, target)
-    #             path_cost = min(path_cost_1, path_cost_2)
-    #             if path_cost > max_path_cost:
-    #                 max_path_cost = path_cost
-    # else:
-    #     for coin in state.remaining_coins:
-    #         path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
-    #         if path_cost > max_path_cost:
-    #             max_path_cost = path_cost
+    #COMMENT: (On my solution)
+    # I used the Manhattan distance as the distance metric.
+    # The distances between the coin_1 to coin_2 to exit are precomputed and ordered.
+    # The true path cost is always less that the max of the above as the above assumes no walls.
+    # The main calculation that is done on every state is the distance between the player and the coin_1,
+    # then max distance between coin_1, coin_2 and exit is done through the precomputed distances.
+    # The max of player to  coin_1 to coin_2 to exit is returned.
+    # note that the (player to coin_1 to coin_2 to exit) is different than (player to cloin_2 to coin_1 to exit) for fixed coins.
+    # thus min of those two is returned.
+    # if only one coin is left the distance (player to coin_1 to exit) is returned.
+    # if no coins are left the distance (player to exit) is returned.
+    #NOTE: I know that this could be easily genralized to any number of coins but I've got enough from this assignment.
 
-    # if max_path_cost == 0:
-    #     max_path_cost = manhattan_distance(source, target)
-    # return max_path_cost
-    #Solution7:
-    # source = state.player
-    # target = problem.layout.exit
-    # max_path_cost = 0
-    # data_size = len(state.remaining_coins)
-
-    # if data_size > 2:
-    #     for coin_1 in state.remaining_coins:
-    #         for coin_2 in state.remaining_coins - {coin_1}:
-    #             for coin_3 in state.remaining_coins - {coin_1, coin_2}:
-    #                 path_cost_1 = manhattan_distance(source, coin_1) + manhattan_distance(coin_1, coin_2) + manhattan_distance(coin_2, coin_3) + manhattan_distance(coin_3, target)
-    #                 path_cost_2 = manhattan_distance(source, coin_1) + manhattan_distance(coin_1, coin_3) + manhattan_distance(coin_3, coin_2) + manhattan_distance(coin_2, target)
-    #                 path_cost_3 = manhattan_distance(source, coin_2) + manhattan_distance(coin_2, coin_1) + manhattan_distance(coin_1, coin_3) + manhattan_distance(coin_3, target)
-    #                 path_cost_4 = manhattan_distance(source, coin_2) + manhattan_distance(coin_2, coin_3) + manhattan_distance(coin_3, coin_1) + manhattan_distance(coin_1, target)
-    #                 path_cost_5 = manhattan_distance(source, coin_3) + manhattan_distance(coin_3, coin_1) + manhattan_distance(coin_1, coin_2) + manhattan_distance(coin_2, target)
-    #                 path_cost_6 = manhattan_distance(source, coin_3) + manhattan_distance(coin_3, coin_2) + manhattan_distance(coin_2, coin_1) + manhattan_distance(coin_1, target)
-    #                 path_cost = min(path_cost_1, path_cost_2, path_cost_3, path_cost_4, path_cost_5, path_cost_6)
-    #                 if path_cost > max_path_cost:
-    #                     max_path_cost = path_cost
-    # elif data_size > 1:
-    #     for coin_1 in state.remaining_coins:
-    #         for coin_2 in state.remaining_coins - {coin_1}:
-    #             path_cost_1 = manhattan_distance(source, coin_1) + manhattan_distance(coin_1, coin_2) + manhattan_distance(coin_2, target)
-    #             path_cost_2 = manhattan_distance(source, coin_2) + manhattan_distance(coin_1, coin_2) + manhattan_distance(coin_1, target)
-    #             path_cost = min(path_cost_1, path_cost_2)
-    #             if path_cost > max_path_cost:
-    #                 max_path_cost = path_cost
-    # else:
-    #     for coin in state.remaining_coins:
-    #         path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
-    #         if path_cost > max_path_cost:
-    #             max_path_cost = path_cost
-    
-    # if max_path_cost == 0:
-    #     max_path_cost = manhattan_distance(source, target)
-    # return max_path_cost
-    #Solution8:
-    # source = state.player
-    # target = problem.layout.exit
-    # max_path_cost = 0
-    # data_size = len(state.remaining_coins)
-    # list_of_coins = list(state.remaining_coins)
-    # if data_size > 2:
-    #     for i in range(data_size):
-    #         for j in range(i+1, data_size):
-    #             for k in range(j+1, data_size):
-    #                 path_cost_1 = manhattan_distance(source, list_of_coins[i]) + manhattan_distance(list_of_coins[i], list_of_coins[j]) + manhattan_distance(list_of_coins[j], list_of_coins[k]) + manhattan_distance(list_of_coins[k], target)
-    #                 path_cost_2 = manhattan_distance(source, list_of_coins[i]) + manhattan_distance(list_of_coins[i], list_of_coins[k]) + manhattan_distance(list_of_coins[k], list_of_coins[j]) + manhattan_distance(list_of_coins[j], target)
-    #                 path_cost_3 = manhattan_distance(source, list_of_coins[j]) + manhattan_distance(list_of_coins[j], list_of_coins[i]) + manhattan_distance(list_of_coins[i], list_of_coins[k]) + manhattan_distance(list_of_coins[k], target)
-    #                 path_cost_4 = manhattan_distance(source, list_of_coins[j]) + manhattan_distance(list_of_coins[j], list_of_coins[k]) + manhattan_distance(list_of_coins[k], list_of_coins[i]) + manhattan_distance(list_of_coins[i], target)
-    #                 path_cost_5 = manhattan_distance(source, list_of_coins[k]) + manhattan_distance(list_of_coins[k], list_of_coins[i]) + manhattan_distance(list_of_coins[i], list_of_coins[j]) + manhattan_distance(list_of_coins[j], target)
-    #                 path_cost_6 = manhattan_distance(source, list_of_coins[k]) + manhattan_distance(list_of_coins[k], list_of_coins[j]) + manhattan_distance(list_of_coins[j], list_of_coins[i]) + manhattan_distance(list_of_coins[i], target)
-    #                 path_cost = min(path_cost_1, path_cost_2, path_cost_3, path_cost_4, path_cost_5, path_cost_6)
-    #                 if path_cost > max_path_cost:
-    #                     max_path_cost = path_cost
-    # elif data_size > 1:
-    #     for i in range(data_size):
-    #         for j in range(i+1, data_size):
-    #             path_cost_1 = manhattan_distance(source, list_of_coins[i]) + manhattan_distance(list_of_coins[i], list_of_coins[j]) + manhattan_distance(list_of_coins[j], target)
-    #             path_cost_2 = manhattan_distance(source, list_of_coins[j]) + manhattan_distance(list_of_coins[j], list_of_coins[i]) + manhattan_distance(list_of_coins[i], target)
-    #             path_cost = min(path_cost_1, path_cost_2)
-    #             if path_cost > max_path_cost:
-    #                 max_path_cost = path_cost
-    # else:
-    #     for coin in state.remaining_coins:
-    #         path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
-    #         if path_cost > max_path_cost:
-    #             max_path_cost = path_cost
-    
-    # if max_path_cost == 0:
-    #     max_path_cost = manhattan_distance(source, target)
-    # return max_path_cost
-    #Solution9:
-    # source = state.player
-    # target = problem.layout.exit
-    # max_path_cost = 0
-    # data_size = len(state.remaining_coins)
-    # list_of_coins = list(state.remaining_coins)
-
-    # if data_size > 1:
-    #     for i in range(data_size):
-    #         for j in range(i+1, data_size):
-    #             path_cost_1 = manhattan_distance(source, list_of_coins[i]) + manhattan_distance(list_of_coins[i], list_of_coins[j]) + manhattan_distance(list_of_coins[j], target)
-    #             path_cost_2 = manhattan_distance(source, list_of_coins[j]) + manhattan_distance(list_of_coins[j], list_of_coins[i]) + manhattan_distance(list_of_coins[i], target)
-    #             path_cost = min(path_cost_1, path_cost_2)
-    #             if path_cost > max_path_cost:
-    #                 max_path_cost = path_cost
-    # else:
-    #     for coin in state.remaining_coins:
-    #         path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
-    #         if path_cost > max_path_cost:
-    #             max_path_cost = path_cost
-    
-    # if max_path_cost == 0:
-    #     max_path_cost = manhattan_distance(source, target)
-    # return max_path_cost
-    #Solution10:
     source = state.player
     target = problem.layout.exit
-    data_size = len(state.remaining_coins)
-    
+    #COMMENT: (The one time calculations)
     if not problem.cache():
         for coin_1 in state.remaining_coins:
             problem.cache()[coin_1]=[]
@@ -202,7 +46,9 @@ def strong_heuristic(problem: DungeonProblem, state: DungeonState) -> float:
             problem.cache()[coin_1].sort(key=lambda x: manhattan_distance(coin_1, x)+manhattan_distance(x, target))
             problem.cache()[coin_1].reverse()
     
+    data_size = len(state.remaining_coins)
     max_path_cost = 0
+    #COMMENT: Picking the longest 2 coins path
     if data_size > 1:
         for coin_1 in state.remaining_coins:
             for coin_2 in problem.cache()[coin_1]:
@@ -213,13 +59,15 @@ def strong_heuristic(problem: DungeonProblem, state: DungeonState) -> float:
             path_cost = min(path_cost_1, path_cost_2)
             if path_cost > max_path_cost:
                 max_path_cost = path_cost
+    #COMMENT: Picking the only 1 coins path if only 1 coin is left
     else:
         for coin in state.remaining_coins:
             path_cost = manhattan_distance(source, coin) + manhattan_distance(coin, target)
             if path_cost > max_path_cost:
                 max_path_cost = path_cost
-    
+    #COMMENT: Picking the path to the exit if no coins are left
     if max_path_cost == 0:
         max_path_cost = manhattan_distance(source, target)
     return max_path_cost
+
 
--- Problem Set 1/search.py
+++ Problem Set 1/search.py
@@ -2,8 +2,8 @@ from problem import HeuristicFunction, Problem, S, A, Solution
 from collections import deque
 from helpers import utils
 
-from queue import PriorityQueue
-from dataclasses import dataclass
+from queue import PriorityQueue #COMMENT: For the UCS, A* and GBFS
+from dataclasses import dataclass #COMMENT: For defining the nodes data type(state, path, cost)
 
 
 # All search functions take a problem and a state
@@ -16,13 +16,14 @@ from dataclasses import dataclass
 
 def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     @dataclass()
+    #COMMENT: Only need to keep track of the state and the path
     class Node:
         state: S
         path: list
         def __eq__(self, other):
             return self.state == other.state
     
-    frontier = deque()
+    frontier = deque()#COMMENT: Regular queue
     explored = set()
     initial_node = Node(initial_state, [])
     if problem.is_goal(initial_state):
@@ -37,27 +38,30 @@ def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
             successor_path = path + [action]
             successor_node = Node(successor, successor_path)
             if successor not in explored and successor_node not in frontier:
-                if problem.is_goal(successor):
+                if problem.is_goal(successor):#COMMENT: Check if the successor is the goal before dequeuing it
                     return successor_path
                 frontier.append(successor_node)
     return None
 
 def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     @dataclass()
+    #COMMENT: Only need to keep track of the state and the path
     class Node:
         state: S
         path: list
         def __eq__(self, other):
             return self.state == other.state
     
-    frontier = deque()
+    frontier = deque()#COMMENT: Regular stack
     explored = set()
     initial_node = Node(initial_state, [])
     frontier.append(initial_node)
     while frontier:
         node = frontier.pop()
         state, path = node.state, node.path
-        if state not in explored and problem.is_goal(state):
+        
+        #COMMENT: the first check to make it pass the test cases that was giving problems {the ones that was later modified} (and generaly any additional check is for the same resnon)
+        if state not in explored and problem.is_goal(state):#COMMENT: Check if the successor is the goal after popping it
             return path
         explored.add(state)
         for action in problem.get_actions(state):
@@ -69,11 +73,13 @@ def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     return None
 
 def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
+    #COMMENT: Now an aditional cost is needed to keep track of the path cost, order is also needed to keep
+    #track of the order of the nodes in the frontier (To pass the given cases)
     @dataclass()
     class Node:
         state: S
         path: list
-        cost: int
+        cost: int#COMMENT: Path cost
         order = 0
         def __gt__(self, other):
             if self.cost > other.cost:
@@ -83,15 +89,16 @@ def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
             return False
         def __eq__(self, other):
             return self.state == other.state
-    order = 0
-    frontier = PriorityQueue()
+    
+    order = 0#COMMENT: To keep track of the order of the nodes entering the frontier
+    frontier = PriorityQueue()#COMMENT: Priority queue based on the cost(path cost)
     explored = set()
     initial_node = Node(initial_state, [], 0)
     frontier.put(initial_node)
     while frontier.queue:
         node = frontier.get()
         state, path, cost = node.state, node.path, node.cost
-        if problem.is_goal(state):
+        if problem.is_goal(state):#COMMENT: Check the node after getting it from the frontier
             return path
         explored.add(state)
         for action in problem.get_actions(state):
@@ -106,17 +113,18 @@ def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
             elif successor_node in frontier.queue:
                 index = frontier.queue.index(successor_node)
                 existing_node = frontier.queue[index]
-                if successor_node < existing_node:
+                if successor_node < existing_node:#COMMENT: Replace the node if the new one has a lower cost
                     frontier.queue[index] = successor_node
     return None
 
 def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
+    #COMMENT: Now cost has two typespath cost
     @dataclass()
     class Node:
         state: S
         path: list
-        path_cost: int
-        cost: int
+        path_cost: int#COMMENT: Path cost
+        cost: int#COMMENT: Path cost + heuristic cost (is the one used to order the nodes in the frontier)
         order = 0
         def __gt__(self, other):
             if self.cost > other.cost:
@@ -127,7 +135,7 @@ def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFu
         def __eq__(self, other):
             return self.state == other.state
 
-    frontier = PriorityQueue()
+    frontier = PriorityQueue()#COMMENT: Priority queue based on the cost(path cost + heuristic cost)
     explored = set()
     initial_node = Node(initial_state, [], 0, 0)
     frontier.put(initial_node)
@@ -135,14 +143,14 @@ def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFu
     while frontier.queue:
         node = frontier.get()
         state, path, path_cost = node.state, node.path, node.path_cost
-        if problem.is_goal(state):
+        if problem.is_goal(state):#COMMENT: Check the node after getting it from the frontier
             return path
         explored.add(state)
         for action in problem.get_actions(state):
             successor = problem.get_successor(state, action)
             successor_path = path + [action]
             successor_path_cost = path_cost + problem.get_cost(state, action)
-            successor_cost = heuristic(problem, successor) + successor_path_cost
+            successor_cost = heuristic(problem, successor) + successor_path_cost#COMMENT: Total cost
             successor_node = Node(successor, successor_path, successor_path_cost, successor_cost)
             successor_node.order = order
             order += 1
@@ -151,16 +159,17 @@ def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFu
             elif successor_node in frontier.queue:
                 index = frontier.queue.index(successor_node)
                 existing_node = frontier.queue[index]
-                if successor_node < existing_node:
+                if successor_node < existing_node:#COMMENT: Replace the node if the new one has a lower cost
                     frontier.queue[index] = successor_node
     return None
 
 def BestFirstSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
+    #COMMENT: Now cost is only one type again but it is the heuristic cost
     @dataclass()
     class Node:
         state: S
         path: list
-        cost: int
+        cost: int #COMMENT: Heuristic cost
         order = 0
         def __gt__(self, other):
             if self.cost > other.cost:
@@ -168,10 +177,10 @@ def BestFirstSearch(problem: Problem[S, A], initial_state: S, heuristic: Heurist
             if self.cost == other.cost:
                 return self.order > other.order
             return False
-        def __eq__(self, other):#for checking if node is in frontier
+        def __eq__(self, other):
             return self.state == other.state
 
-    frontier = PriorityQueue()
+    frontier = PriorityQueue()#COMMENT: Priority queue based on the cost(heuristic cost)
     explored = set()
     initial_node = Node(initial_state, [], 0)
     order = 0
@@ -194,6 +203,6 @@ def BestFirstSearch(problem: Problem[S, A], initial_state: S, heuristic: Heurist
             elif successor_node in frontier.queue:
                 index = frontier.queue.index(successor_node)
                 existing_node = frontier.queue[index]
-                if successor_node < existing_node:
+                if successor_node < existing_node:#COMMENT: Replace the node if the new one has a lower cost
                     frontier.queue[index] = successor_node
     return None
